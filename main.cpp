694e8c62ec0ef2e463cb024009def7c9c60c3f9c
Angry_Programist <Kirill_071@bk.ru>
2021-03-06 13:56:43 +0300

    final commit

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ private:
     string type;
     string dough;
     string sauce;
-    float price;
+    const float price;
 public:
-    Pizza(){}
-    //Pizza(string m_type="0", string m_dough="0", string m_sauce="0", float m_price=0) : 
-    //    type{m_type}, dough{m_dough}, sauce{m_sauce}, price{m_price} {}
     
+    Pizza( string m_type, float m_price): price(m_price), type(m_type) {
+
+    }
     virtual void GetPrice() {
         cout <<"Price of " << type << " = " << price << endl;
     }
@@ -31,59 +31,26 @@ public:
 };
 
 class Peperoni : public Pizza {
-private:
-    float price = 1000;
-    string type = "Peperoni";
+
 public:
-    Peperoni(){}
-    //Peperoni(string m_type="0", string m_dough="0", string m_sauce="0", float m_price=0) : 
-    //   Pizza(m_type, m_dough, m_sauce, m_price) {}
+
+    Peperoni(): Pizza("Peperoni" , 1000){};
     
-    void GetPrice() override{
-        cout <<"Price of " << type << " = " << price << endl;
-    }
-
-    int ReturnPrice() override {
-        return price;
-    }
-
-    void Prepare(){
-        cout << type << " is prepared " << endl;
-    }
 };
 
 class Four_Cheese : public Pizza {
-private:
-    float price = 900;
-    string type = "Four_Cheese";
+
 public:
-    Four_Cheese(){}
-    //Four_Cheese(string m_type="0", string m_dough="0", string m_sauce="0", float m_price=0) : 
-    //    Pizza(m_type, m_dough, m_sauce, m_price) {}
-    
-    void GetPrice() override{
-        cout <<"Price of " << type << " = " << price << endl;
-    }
-    int ReturnPrice() override {
-        return price;
-    }
+
+    Four_Cheese(): Pizza("Four_cheese" , 900){};
+
 };
 
 class BBQ : public Pizza {
-private:
-    float price = 950;
-    string type = "BBQ";
 public:
-    BBQ(){}
-    //BBQ(string m_type="0", string m_dough="0", string m_sauce="0", float m_price=0) : 
-    //   Pizza(m_type, m_dough, m_sauce, m_price) {}
+
+    BBQ(): Pizza("BBQ" , 950){};
     
-    void GetPrice() override{
-        cout <<"Price of " << type << " = " << price << endl;
-    }
-    int ReturnPrice() override {
-        return price;
-    }
 };
 
 void menu(){
@@ -177,12 +144,6 @@ void Order(){
 
 int main()
 {
-    //Peperoni a;
-    //a.Prepare();
-    //a.GetPrice();
-
     Order();
-
- 
     return 0;
 }
